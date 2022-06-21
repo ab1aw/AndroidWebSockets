3b223f9a4763c4ac9a457661140cadce1be5da74
Mike Polia <mpolia@medacuitysoftware.com>
2022-06-21 14:38:07 -0400

    Successfully reading the neighbor's IP address.

--- app/src/main/java/com/example/websocket/MainActivity.java
+++ app/src/main/java/com/example/websocket/MainActivity.java
@@ -16,10 +16,16 @@ import org.json.JSONObject;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.LinkProperties;
 import android.net.Network;
 import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdManager.DiscoveryListener;
+import android.net.nsd.NsdServiceInfo;
 import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
@@ -29,20 +35,29 @@ import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Iterator;
 import java.util.Set;
 
+import java.io.InputStreamReader;
+
+
 public class MainActivity extends AppCompatActivity {
 
-    String tag = "MY_APP";
+    public String tag = "MY_APP";
     WrBlockingSocket c1;
 
     String debugMessage = "AbhiAndroid\n";
 
     TextView textView;
+    private static Context context;
 
-    private final Handler myTextHandler = new Handler(new Handler.Callback() {
+    public final Handler myTextHandler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message stringMessage) {
             textView.append((String) stringMessage.obj);
@@ -55,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        MainActivity.context = getApplicationContext();
+        
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.simpleTextView);
         textView.setMovementMethod(new ScrollingMovementMethod());
@@ -64,7 +81,87 @@ public class MainActivity extends AppCompatActivity {
         new Thread() {
             public void run() {
 
+                Log.v(tag, "Step one");
+                IntentFilter filter = new IntentFilter("android.net.conn.TETHER_STATE_CHANGED");
+                MainActivity.registerReceiver(this, filter);
+
+
                 try {
+
+                    // This does not work.  It gets stuck:
+                    //getTetheringInfo(tag);
+
+                    {
+                        Log.v(tag, "Step two");
+                        Runtime runtime = Runtime.getRuntime();
+                        Process proc = runtime.exec("ip neigh show");
+                        proc.waitFor();
+                        BufferedReader reader;
+                        reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            Log.v(tag, "Step two B");
+
+                            String[] clientInfo = line.split(" +");
+                            if(!clientInfo[3].equalsIgnoreCase("type")) {
+                                Log.v(tag, "Step two C");
+                                String mac = clientInfo[3];
+                                String ip = clientInfo[0];
+                                String msg = "ip: " + ip + " Mac: " + mac;
+                                Log.d(tag, "IP : " + ip);
+                                Log.d(tag, "Mac : " + mac);
+
+                                Message stringMessage = Message.obtain(myTextHandler);
+                                stringMessage.obj = "\nreader got this = " + msg;
+                                stringMessage.sendToTarget();
+                            }
+                        }
+
+                    }
+
+                    /*
+                    // Does not work in newer versions of Android;  10+
+                    {
+                        Log.v(tag, "Step two");
+                        Message stringMessage = Message.obtain(myTextHandler);
+                        stringMessage.obj = "\ngetUSBThetheredIP() = " + getUSBThetheredIP();
+                        stringMessage.sendToTarget();
+                    }
+*/
+
+
+                    {
+                        Log.v(tag, "Step three");
+                        Message stringMessage = Message.obtain(myTextHandler);
+                        stringMessage.obj = "\nUtils.getMACAddress(wlan0) = " + Utils.getMACAddress("wlan0");
+                        stringMessage.sendToTarget();
+                    }
+
+                    {
+                        Log.v(tag, "Step four");
+                        Message stringMessage = Message.obtain(myTextHandler);
+                        stringMessage.obj = "\nUtils.getMACAddress(eth0) = " + Utils.getMACAddress("eth0");
+                        stringMessage.sendToTarget();
+                    }
+
+                    {
+                        Log.v(tag, "Step five");
+                        Message stringMessage = Message.obtain(myTextHandler);
+                        stringMessage.obj = "\nUtils.getIPAddress(true) = " + Utils.getIPAddress(true);
+                        stringMessage.sendToTarget();
+                    }
+
+                    {
+                        Message stringMessage = Message.obtain(myTextHandler);
+
+                        Process p = Runtime.getRuntime().exec("/system/bin/ip address");
+
+                        stringMessage.obj = "\ngetOutputStream = " + p.getOutputStream().toString();
+                        stringMessage.sendToTarget();
+                    }
+
+
                     Log.v(tag, "call executeCommand()");
                     {
                         Message stringMessage = Message.obtain(myTextHandler);
@@ -240,4 +337,188 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
+
+    private static void registerReceiver(Thread thread, IntentFilter filter) {
+        Log.v("MY_APP", "call registerReceiver()");
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Got getUSBThetheredIP from:
+    // https://stackoverflow.com/questions/11506160/how-to-get-the-system-ip-address-after-usb-tethering-of-android-phone
+
+    public static String getUSBThetheredIP() {
+        Log.v("MY_APP", "getUSBThetheredIP() #1");
+
+        BufferedReader bufferedReader = null;
+        String ips="";
+
+        try {
+            Log.v("MY_APP", "getUSBThetheredIP() #2");
+            bufferedReader = new BufferedReader(new FileReader("/proc/net/arp"));
+            Log.v("MY_APP", "getUSBThetheredIP() #3");
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                Log.v("MY_APP", "getUSBThetheredIP() #4...");
+                String[] splitted = line.split(" +");
+                if (splitted != null && splitted.length >= 4) {
+                    String ip = splitted[0];
+                    String mac = splitted[3];
+                    if (mac.matches("..:..:..:..:..:..")) {
+                        if (mac.matches("00:00:00:00:00:00")) {
+                            Log.d("MY_APP", "Wrong:" + mac + " : " + ip);
+                        } else {
+                            Log.d("MY_APP", "Correct:" + mac + " : " + ip);
+                            ips = ip;
+                            //break;
+                        }
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            Log.e("MY_APP", "\nEXCEPTION! " + e.getMessage() + "\n : " + e.getCause() + "\n : " + e.toString());
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e("MY_APP", "\nEXCEPTION! " + e.getMessage() + "\n : " + e.getCause() + "\n : " + e.toString());
+            e.printStackTrace();
+        } finally{
+            try {
+                Log.v("MY_APP", "getUSBThetheredIP() #5");
+                bufferedReader.close();
+            } catch (IOException e) {
+                Log.e("MY_APP", "\nEXCEPTION! " + e.getMessage() + "\n : " + e.getCause() + "\n : " + e.toString());
+                e.printStackTrace();
+            }
+        }
+        return ips;
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager mgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] info = mgr.getAllNetworkInfo();
+        if (info != null) {
+            for (int i = 0; i < info.length; i++) {
+                if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+/*
+    public void initializeDiscoveryListener() {
+
+        // Instantiate a new DiscoveryListener
+        DiscoveryListener discoveryListener = new NsdManager.DiscoveryListener() {
+
+            // Called as soon as service discovery begins.
+            @Override
+            public void onDiscoveryStarted(String regType) {
+                Log.d("MY_APP", "Service discovery started");
+            }
+
+            @Override
+            public void onServiceFound(NsdServiceInfo service) {
+                // A service was found! Do something with it.
+                Log.d("MY_APP", "Service discovery success : " + service);
+                Log.d("MY_APP", "Service Type is : " + service.getServiceType());
+            }
+
+            @Override
+            public void onServiceLost(NsdServiceInfo service) {
+                // When the network service is no longer available.
+                // Internal bookkeeping code goes here.
+                Log.e("MY_APP", "service lost: " + service);
+            }
+
+            @Override
+            public void onDiscoveryStopped(String serviceType) {
+                Log.i("MY_APP", "Discovery stopped: " + serviceType);
+            }
+
+            @Override
+            public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+                Log.e("MY_APP", "Discovery failed: Error code:" + errorCode);
+            }
+
+            @Override
+            public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+                Log.e("MY_APP", "Discovery failed: Error code:" + errorCode);
+            }
+        };
+    }
+*/
+
+
+    // From here:
+    // https://android.develop-bugs.com/article/25534620/Detect+USB+tethering+on+android
+    public void getTetheringInfo(String tag) {
+        ConnectivityManager cm =
+                (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        Log.d(tag,"test enable usb tethering 1");
+        String[] available = null;
+        int code=-1;
+        Method[] wmMethods = cm.getClass().getDeclaredMethods();
+        Log.d(tag,"test enable usb tethering 2");
+        for(Method method: wmMethods){
+            Log.d(tag,"test enable usb tethering 3");
+            if(method.getName().equals("getTetherableIfaces")){
+                Log.d(tag,"test enable usb tethering 4");
+                try {
+                    available = (String[]) method.invoke(cm);
+                    break;
+                } catch (IllegalArgumentException e) {
+// TODO Auto-generated catch block
+                    e.printStackTrace();
+                    Log.e("MY_APP", "\nEXCEPTION! " + e.getMessage() + "\n : " + e.getCause() + "\n : " + e.toString());
+                } catch (IllegalAccessException e) {
+// TODO Auto-generated catch block
+                    e.printStackTrace();
+                    Log.e("MY_APP", "\nEXCEPTION! " + e.getMessage() + "\n : " + e.getCause() + "\n : " + e.toString());
+                } catch (InvocationTargetException e) {
+// TODO Auto-generated catch block
+                    e.printStackTrace();
+                    Log.e("MY_APP", "\nEXCEPTION! " + e.getMessage() + "\n : " + e.getCause() + "\n : " + e.toString());
+                }
+            }
+        }
+
+        Log.d(tag,"test enable usb tethering 5");
+
+        for(Method method: wmMethods){
+            Log.d(tag,"test enable usb tethering 6");
+            if(method.getName().equals("tether")){
+                Log.d(tag,"test enable usb tethering 7");
+                try {
+                    Log.d(tag,"test enable usb tethering 8");
+                    // Gets stuck in the following call:
+                    code = (Integer) method.invoke(cm, available[0]);
+                    Log.d(tag,"test enable usb tethering 9");
+                } catch (IllegalArgumentException e) {
+// TODO Auto-generated catch block
+                    Log.e("MY_APP", "\nEXCEPTION! " + e.getMessage() + "\n : " + e.getCause() + "\n : " + e.toString());
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+// TODO Auto-generated catch block
+                    Log.e("MY_APP", "\nEXCEPTION! " + e.getMessage() + "\n : " + e.getCause() + "\n : " + e.toString());
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+// TODO Auto-generated catch block
+                    Log.e("MY_APP", "\nEXCEPTION! " + e.getMessage() + "\n : " + e.getCause() + "\n : " + e.toString());
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+
+        Log.d(tag,"test enable usb tethering 10");
+
+        if (code==0)
+            Log.d(tag,"Enable usb tethering successfully!");
+        else
+            Log.d(tag,"Enable usb tethering failed!");
+
+    }
 }
---
+++ app/src/main/java/com/example/websocket/NetworkChangeReceiver.java
@@ -0,0 +1,22 @@
+package com.example.websocket;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+
+        int status = NetworkUtil.getConnectivityStatusString(context);
+        Log.e("MY_APP", "Sulod sa network receiver");
+        if ("android.net.conn.CONNECTIVITY_CHANGE".equals(intent.getAction())) {
+            if (status == NetworkUtil.NETWORK_STATUS_NOT_CONNECTED) {
+            } else {
+            }
+        }
+    }
+}
